42bff3ad2b7e7288c504455e8b9bae76ab48b28e
nardusstricta <gabriholz@gmail.com>
2018-01-18 21:33:11 +0100

    add all files

---
+++ dissociation_of_ophos.R
@@ -0,0 +1,113 @@
+####Berechnung des Dissoziationsdiagramm auf Basis der Konzentrationen
+#Gleichgewichtskonstanten:
+pk1 <- 2.16
+pk2 <- 7.21
+pk3 <- 12.32
+#pks-Werte (-log10 von Gleichgewichtskonstante)
+pH <- seq(3, 14, 0.2)
+h <- 10 ^ (-pH) 
+K1 <- 10 ^ (-pk1)
+K2 <- 10 ^ (-pk2)
+K3 <- 10 ^ (-pk3)
+
+#Gesamtkonzentration H3 ermittelt über die Konzentrationssumme 
+H3 <- h^3 + (h^2 * K1) + (h * K1 * K2) + (K1 * K2 * K3)
+
+# Gleichungen der vier Gleichgewichtskurven im Dissoziationsdiagramm
+# Teilen durch die Gesamtkonzentration H3
+RAH3 <- (h^3)/H3
+RAH2 <- h^2 * K1 / H3
+RAH <- h * K1 * K2/H3
+RA <- K1 * K2 * K3 / H3
+plot(pH, RAH3, type="l", ylim=c(0,1), ylab = "Konzentrationsanteile", main = "Dissoziationsdiagramm der Phosphorsäure")
+points(pH, RAH2, type = "l", col="red")
+points(pH, RAH, type = "l", col ="blue", lty=2)
+points(pH, RA, type = "l", col = "green", lty=2)
+legend("topright", c(expression("H"["3"]*"PO"["4"]), expression("H"["2"]*"PO"["4"]^"-"), expression("HPO"["4"]^"2-"), expression("PO"["4"]^"3-")), col = c("black", "red", "blue", "green"), lty = c(1,1,2,2), bty = "n")
+
+###
+#Funktion des gelösten Phosphatgehaltes im Gelichgewicht mit Hydroxylapatit###########
+#bei gegeben pH und ca+ aktivität in mol/l
+###
+#Idee: Löslichkeitsprodukt (Ksp) formolieren aus der Gleichgewichtsreaktion von Hydroxylapatit (Ca5(PO4)3OH + 7 H+ <-> 5 Ca2+ + 3H2PO4- + H2O); Ksp = a5 Ca2+ * a3 H2PO4- / a7 H+ (a = aktivität)
+#Nach umformen und einsetzen von Ksp = 14.46 ->
+#log(aH2PO4-) = 1/3 (14.46 - 7 *pH - 5 log aCa2+)
+HPO2 <- function(ph, gammaca = 0.0025){
+  erg <- 1/3 * (14.46 - 5 * log10(gammaca) - (7 * ph))
+  erg <- 10^(erg) * 30.973762 #Umrechung von Mol/l zu Gramm/l
+  return(erg *1000) #In Miligramm *1000
+}
+##
+#Beispiel#####
+##
+HPO2(5)
+HPO2(6)
+ca_beispiel <- seq(0.0001, 0.009, 0.0001)
+pH_beispiel <- seq(3, 10, 0.01)
+erg1 <- sapply(pH_beispiel, HPO2, gammaca = 0.0025) #Bei gleichen Ca2+
+plot(pH_beispiel, log10(erg1), type = "l", ylab = "Gelöster Phosphor log(mg/l)", xlab = " pH")
+#Mit Veränderung der Ca2+ Aktivität
+mat_erg <- as.data.frame(matrix(0,length(ca_beispiel), length(pH_beispiel)))
+rownames(mat_erg) <- ca_beispiel
+colnames(mat_erg) <- pH_beispiel
+for (i in 1:length(ca_beispiel)){
+  for (j in 1:length(pH_beispiel)){
+    mat_erg[i,j] <- HPO2(ph = pH_beispiel[j],  gammaca = ca_beispiel[i])
+  }
+}
+#mat_erg$`5.01`[which(ca_beispiel==0.0025)]
+mat_erg <- rotate(mat_erg) # rotation der Matrix 
+filled.contour(pH_beispiel, ca_beispiel,   log10(mat_erg), ylab=expression("Ca"^"2+"*"Aktivität in mol l"^"-1"), xlab=("pH-Wert"), main  = "Phosphor in Lösung log(mg/l)", color = terrain.colors)
+
+#Phosphorgehalt Abhängig von der CO2 Konzentration############
+CO2_PD <- seq(0.1, 5, 0.1) #CO2 Patialdrücke  in kPA
+ph_co2 <- as.data.frame(matrix(0,length(CO2_PD),8)) # Matrix für die Aktivitäten von Ca2+ und den dazugehörigen pH-Wert
+colnames(ph_co2) <- c("ca2", "CaHCO", "H", "HCO3", "CO2", "OH", "PH", "paco2")
+
+#Berechung der Aktivitäten und pH-Werten anhand des Calciumsmodelles mit verschienen  CO2 Patialdrücken  
+for (i in 1:length(CO2_PD)){
+  ph_co2[i,] <- co2pa_ca(pco02=CO2_PD[i], temp1=20)
+  print(CO2_PD[i])
+}
+ph_co2
+cacons<-numeric(length(CO2_PD)) #Berechnung der Ca2+ Konzentrationen aus den Aktivitäten 
+for (i in 1:length(CO2_PD)){
+  cacons[i] <- ca2(pco02=ph_co2$paco2[i], temp1=20, ph=ph_co2$PH[i], ga=ph_co2$ca2[i])
+}
+
+plot(ph_co2$paco2, cacons * 40 *1000, type = "l", ylab = expression("Ca" ^ "2+" * " Konzentration in mg" ^ "-1"), xlab = expression("CO" ["2"] * " Patialdruck kPa"))
+
+#Berechung der Phosphatgehalte mit den ca2+ Konzentrationen und dem pH-Wert im Gleichgewicht
+plot(ph_co2$PH, cacons, type = "l", xlab = "pH", ylab = expression("ca"^"2+"*"  mol l"^"-1"))
+phos <- HPO2(ph = ph_co2$PH, gammaca = cacons)
+plot(ph_co2$PH, log10(phos), type = "l")
+
+phos <- as.data.frame(cbind(phos, ph_co2$PH, cacons, CO2_PD))
+names(phos) <- c("Phosph", "pH", "Ca2+", "CO2_Patial")
+
+library(ggplot2)
+p <- ggplot(phos, aes(pH, Phosph, color = CO2_Patial))
+  p + geom_point()
+
+
+
+plot(CO2_PD, log10(cacons), type = "l", xlab = "Patialdruck CO2", ylab = "Ca2+ Konzentration mol/l")
+#Plot aus Lindsy
+phos_03 <- HPO2(ph_co2$PH[1], cacons[1])
+##
+##Veränderung des pH-Wertes#########
+##
+#Phosphorgehalt Abhängig von der CO2 Konzentration############
+PH_PD <- seq(5, 9, 0.2) #CO2 Patialdrücke  in kPA 
+#Berechung der Aktivitäten und pH-Werten anhand des Calciumsmodelles mit verschienen  CO2 Patialdrücken  
+ph_co2_3 <- sapply(PH_PD, pH_ca_fun, temp1 = 20)
+ph_co2_3 <- as.data.frame(t(ph_co2_3))
+colnames(ph_co2_3) <- c("ca2", "CaHCO", "H", "HCO3", "CO2", "OH", "PH", "paco2")
+cacons_2 <- numeric(length(PH_PD))
+for (i in 1:length(PH_PD)){
+  cacons_2[i] <- ca2(pco02=ph_co2_3$paco2[i], temp1=20, ph=ph_co2_3$PH[i], ga=ph_co2_3$ca2[i])
+}
+phos_2_pH <- HPO2(ph = ph_co2_3$PH, gammaca = cacons_2)
+plot(PH_PD, log10(phos_2_pH), type = "l")
+plot(ph_co2$PH, log10(phos), type = "l")
+
---
+++ rotation_fun.R
@@ -0,0 +1,154 @@
+matx <- matrix(1:12, 3, 4)
+matx[3,2] <- 19
+matx
+image(matx)
+rotate <- function(x) t(apply(x, 2, rev))
+imag <- function(m){
+  imag
+}
+image(rotate(matx))
+
+#Carroll Funktiton------------------
+carl <- function(temp=20){
+  tempK <- 273.15+temp # in Grad Celsius
+  newH <- exp((-6.8346)+((1.2817*10^4)/tempK)-
+                ((3.7668*10^6)/tempK^2)+((2.997*10^8)/(tempK^3))) 
+  newkpa <- 1/newH   #Umrechen von Mg Pa auf Kg Pa
+  newkpa <- newkpa/1000 
+  newkpa <- newkpa*(998/18)
+  return(newkpa)
+}
+#Dissoziationstufen der 6 Carbonat Spezies-------------------
+#1
+ca2 <- function(pco02, temp1, ph, ga){
+  patial_co24 <- (10^(8.28 - log10(carl(temp=temp1)))*((10^-ph)^2))/(pco02*ga)
+  return(patial_co24)
+}
+#2
+cahco3 <- function(pco02, temp1, ph, ga){
+  patial_co25 <- ((10^3.02)*(10^-ph))/ga
+  return(patial_co25)
+}
+#3
+H <- function(pco02, temp1, ph, ga){
+  patial_co21<-(10^-ph)/ga
+  return(patial_co21)
+}
+#4
+HCo3 <- function(pco02, temp1, ph, ga){
+  patial_co22 <- (10^(-6.36+log10(carl(temp=temp1)))*pco02)/(ga*(10^-ph))
+  return(patial_co22)
+}
+#5
+co23 <- function(pco02, temp1, ph, ga){
+  patial_co23 <- (10^(-16.69+log10(carl(temp=temp1)))*pco02)/(ga*((10^-ph)^2))
+  return(patial_co23)
+}
+#6
+oh <- function(pco02, temp1, ph, ga){
+  patial_co26 <- (10^-14)/((10^-ph)*ga)
+  return(patial_co26)
+}
+#Elektroneutralit?tsformel als Funktion (Dividiert durch die jeweiligen Aktivit?tskoeffizienten usw.)------------------
+sumCA <- function(pco02, temp1, ph, ga){
+  sumCAer <- (2*ca2(pco02=pco02, temp1=temp1, ph=ph, ga=ga[1]))+
+    cahco3(pco02=pco02, temp1=temp1, ph=ph, ga=ga[2])+
+    H(pco02=pco02, temp1=temp1, ph=ph, ga=ga[3])-
+    HCo3(pco02=pco02, temp1=temp1, ph=ph, ga=ga[4])-
+    (2*co23(pco02=pco02, temp1=temp1, ph=ph, ga=ga[5]))-
+    oh(pco02=pco02, temp1=temp1, ph=ph, ga=ga[6])
+  return(sumCAer)
+}
+#Beispiel
+ga <- rep(1, 6)#Gamma wird als Vektor ?bergeben
+sumCA(pco02=1, temp=20, ph=7, ga=ga) #0.008107546
+
+#Ionenst?rke errechen um neuen  Aktivit?tskoeffizienten Gamma zu bekommen--------
+phmu<-function(pco02=pco02, temp1=temp1, ph=ph, ga=ga){
+  #Ionenstärke mu 
+  mu <- 0.5*(ca2(pco02=pco02, temp1=temp1, ph=ph,ga=ga[1])*
+               4+cahco3(pco02=pco02, temp1 =temp1, ph=ph,ga=ga[2])+
+               H(pco02=pco02, temp1=temp1, ph=ph,ga=ga[3])+
+               HCo3(pco02=pco02, temp1=temp1, ph=ph,ga=ga[4])+
+               co23(pco02=pco02, temp1=temp1, ph=ph,ga=ga[5])*
+               4+oh(pco02=pco02, temp1=temp1, ph=ph,ga=ga[6]))
+  #temperaturabhängige Koeffizienten usw
+  A<-0.504
+  B<-0.327e+8
+  d<-c(6e-8, 3e-8, 9e-8, 4e-8, 4.5e-8, 3.5e-8)
+  z<-c(2,1,1,1,2,1)
+  #Vektor f?r Gamma erstellen und durch Debye-H?ckel-Formel neu errechen
+  ga<-numeric(6) 
+  for(i in 1:6){
+    ga[i] <- 10^(((-A*z[i]^2) * sqrt(mu))/(1+B * d[i] * (sqrt(mu))))
+  }
+  return(ga) 
+}
+
+#Wir haben 6 unbekannte Spezies, wir kennen PCO2 und kennen 7 Zusammenh?nge-----
+#Gleichungen -> versuchen zu l?sen
+co2pa_ca <- function(pco02, temp1){#Funktion braucht Patialdurck und Temperatur
+  ga <- rep(1,6) #Gamma anfangs immer auf 1 und PH auf 7
+  ph <- 7
+  repeat{
+    ph1 <- ph
+    Int <- 7
+    repeat{
+      er <- sumCA(pco02=pco02, temp1=temp1, ph=ph, ga=ga)
+      if(abs(er) < 1e-8|Int==0){
+        break
+      }else{
+        Int <- Int/2
+        if(er > 0){
+          ph<-ph+Int
+        }else{
+          ph <- ph - Int}
+      }
+    }
+    if(abs(ph1-ph)<1/1000){#f?r schnellere Durchl?ufe reichen auch 1000
+      return(c(ga,ph, pco02)) #Bevor auch die zweite repeat Schleife
+      #abricht werden 6*Gammas, PH, und der CO2-Patialdruck ausgeben
+      break
+    }else{
+      ga <- phmu(pco02=pco02, temp1=temp1, ph=ph, ga=ga)#neue Gammas abrufen
+    }
+  }
+}
+#Beispiel
+co2pa_ca(pco02=0.3, temp=10)#0.7729347 0.9341701 0.9407609 0.9353685
+#0.7673852 0.9347748 7.5415645 0.3000000
+
+#Für einen Bekannten pH-Wert und einen unbekannten CO2 Patialdruck#####
+#Wir haben 6 unbekannte Spezies, wir kennen PCO2 und kennen 7 Zusammenh?nge-----
+#Gleichungen -> versuchen zu l?sen
+pH_ca_fun <- function(ph, temp1){#Funktion braucht Patialdurck und Temperatur
+  ga <- rep(1,6) #Gamma anfangs immer auf 1 und PH auf 7
+  pco02 <- 2.5
+  repeat{
+    pco02_1 <- pco02
+    Int <- 2.5
+    repeat{
+      er <- sumCA(pco02=pco02, temp1=temp1, ph=ph, ga=ga)
+      if(abs(er) < 1e-8|Int==0){
+        break
+      }else{
+        Int <- Int/2
+        if(er > 0){
+          pco02 <- pco02 + Int
+        }else{
+          pco02 <- pco02 - Int}
+      }
+    }
+    if(abs(pco02_1 - pco02)<1/10000){#f?r schnellere Durchl?ufe reichen auch 1000
+      return(c(ga,ph, pco02)) #Bevor auch die zweite repeat Schleife
+      #abricht werden 6*Gammas, PH, und der CO2-Patialdruck ausgeben
+      break
+    }else{
+      ga <- phmu(pco02=pco02, temp1=temp1, ph=ph, ga=ga)#neue Gammas abrufen
+    }
+  }
+}
+#Beispiel
+pH_ca_fun(ph = 5, temp=10)#0.7729347 0.9341701 0.9407609 0.9353685
+#0.7673852 0.9347748 7.5415645 0.3000000
+##
