23daddd7ac3919e314ec94aa78769eb90fc28db4
nardusstricta <gabriholz@gmail.com>
2018-01-29 11:08:45 +0100

    Temperatur für die Deby-Hückel-Gleichung wurde mit einem linieren Modell angepasst.

--- rotation_fun.R
+++ rotation_fun.R
@@ -1,15 +1,10 @@
-matx <- matrix(1:12, 3, 4)
-matx[3,2] <- 19
-matx
-image(matx)
-rotate <- function(x) t(apply(x, 2, rev))
-imag <- function(m){
-  imag
+kpa_fun <- function(kpa){
+  kpa/101.3
 }
-image(rotate(matx))
 
+#Für die Temperatur wird der Wert temp1 eingegeben, welcher dann über die Funktion verändert werden kann.
 #Carroll Funktiton------------------
-carl <- function(temp=20){
+carl <- function(temp=temp1){
   tempK <- 273.15+temp # in Grad Celsius
   newH <- exp((-6.8346)+((1.2817*10^4)/tempK)-
                 ((3.7668*10^6)/tempK^2)+((2.997*10^8)/(tempK^3))) 
@@ -64,6 +59,22 @@ ga <- rep(1, 6)#Gamma wird als Vektor ?bergeben
 sumCA(pco02=1, temp=20, ph=7, ga=ga) #0.008107546
 
 #Ionenst?rke errechen um neuen  Aktivit?tskoeffizienten Gamma zu bekommen--------
+#Tempereaturabhängigkeit errechen mit folgender Funktion:
+temp_A <- function(Ax){
+  par_a <- c(0.4883, 0.5002, 0.5046, 0.5092, 0.5141, 0.5241, 0.5351, 0.5471, 0.5739)
+  par_temp <- c(0,15,20,25,30,40,50,60,80)
+  fml <- lm(par_a~par_temp)
+  A_ret <- fml$coefficients[2]* Ax + fml$coefficients[1]
+  return(A_ret)
+}
+temp_B <- function(Bx){
+  par_b <- c(3.241^10, 3.267^10, 3.276^10, 3.286 ^10, 3.297^10, 3.318^10,3.341^10,3.366^10, 3.420^10)
+par_temp <- c(0,15,20,25,30,40,50,60,80)
+fmb <- lm(par_b ~ par_temp)
+B_ret <- fmb$coefficients[2]*Bx + fmb$coefficients[1]
+return(B_ret)
+}
+
 phmu<-function(pco02=pco02, temp1=temp1, ph=ph, ga=ga){
   #Ionenstärke mu 
   mu <- 0.5*(ca2(pco02=pco02, temp1=temp1, ph=ph,ga=ga[1])*
@@ -73,8 +84,8 @@ phmu<-function(pco02=pco02, temp1=temp1, ph=ph, ga=ga){
                co23(pco02=pco02, temp1=temp1, ph=ph,ga=ga[5])*
                4+oh(pco02=pco02, temp1=temp1, ph=ph,ga=ga[6]))
   #temperaturabhängige Koeffizienten usw
-  A<-0.504
-  B<-0.327e+8
+  A<-temp_A(temp1)
+  B<-temp_B(temp1)
   d<-c(6e-8, 3e-8, 9e-8, 4e-8, 4.5e-8, 3.5e-8)
   z<-c(2,1,1,1,2,1)
   #Vektor f?r Gamma erstellen und durch Debye-H?ckel-Formel neu errechen
@@ -115,7 +126,7 @@ co2pa_ca <- function(pco02, temp1){#Funktion braucht Patialdurck und Temperatur
   }
 }
 #Beispiel
-co2pa_ca(pco02=0.3, temp=10)#0.7729347 0.9341701 0.9407609 0.9353685
+co2pa_ca(pco02=0.3, temp=25)#0.7729347 0.9341701 0.9407609 0.9353685
 #0.7673852 0.9347748 7.5415645 0.3000000
 
 #Für einen Bekannten pH-Wert und einen unbekannten CO2 Patialdruck#####
@@ -139,7 +150,7 @@ pH_ca_fun <- function(ph, temp1){#Funktion braucht Patialdurck und Temperatur
           pco02 <- pco02 - Int}
       }
     }
-    if(abs(pco02_1 - pco02)<1/10000){#f?r schnellere Durchl?ufe reichen auch 1000
+    if(abs(pco02_1 - pco02)<1/1000){#f?r schnellere Durchl?ufe reichen auch 1000
       return(c(ga,ph, pco02)) #Bevor auch die zweite repeat Schleife
       #abricht werden 6*Gammas, PH, und der CO2-Patialdruck ausgeben
       break
@@ -149,6 +160,6 @@ pH_ca_fun <- function(ph, temp1){#Funktion braucht Patialdurck und Temperatur
   }
 }
 #Beispiel
-pH_ca_fun(ph = 5, temp=10)#0.7729347 0.9341701 0.9407609 0.9353685
+pH_ca_fun(ph = 6.5, temp1=5)#0.7729347 0.9341701 0.9407609 0.9353685
 #0.7673852 0.9347748 7.5415645 0.3000000
 ##
